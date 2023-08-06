d3f65591b66f6b9f89dd4a05fc6b4de2b43c8ba0
codeark1 <ajitenadavid@gmail.com>
2023-08-06 04:05:21 +0100

    just a code refactor
    just a code refactor

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
-const body = document.body;
-const bodyDefault =
-  (body.style.cssText = `display:flex; justify-content:center; align-items: center;height:100vh;flex-direction:column; background-image: url(./sunset.jpg); background-repeat: no-repeat; background-size: cover;`);
-const input = document.createElement("input");
-input.setAttribute("placeholder", "enter a city");
-input.style.cssText = `height:4rem;width:50rem; border-radius:10px; border:none; text-transform:uppercase `;
-body.append(input);
-const h1 = document.createElement("h1");
-h1.style.cssText = `margin-top:5rem`;
-body.append(h1);
-const h2 = document.createElement("h2");
-body.append(h2);
+const btn = document.getElementById("btn")
+const temp = document.getElementById("temp-text")
+apiKey = "c90f1c242ea476609d85ba660d5ba11d";
 
-const apiKey = "c90f1c242ea476609d85ba660d5ba11d";
 
-input.addEventListener("change", (e) => {
+btn.addEventListener("click", (e) => {
   e.preventDefault();
-  let value = e.target.value;
+  const input = document.getElementById("input").value
+  // let value = e.target.value;
 
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${apiKey}&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=metric`
   )
     .then((response) => {
       return response.json();
@@ -26,19 +17,9 @@ input.addEventListener("change", (e) => {
     .then((data) => {
       console.log(data);
       if (data.cod !== "404") {
-        h1.textContent = `The current wearther in  ${value} is ${data.main.temp}°`;
-        h2.textContent = `Humidity: ${data.main.humidity}%`;
+        temp.textContent = `${data.main.temp}°`;
+    
       }
-      if (data.main.temp > 30) {
-        body.style.cssText = `display:flex; justify-content:center; align-items: center;height:100vh;flex-direction:column; background-image: url(./sunny.avif); background-repeat: no-repeat; background-size: cover;`;
-        
-      } else {
-        body.style.cssText = bodyDefault;
-      }
-
-      if (data.main.temp <= 10) {
-        body.style.cssText = `display:flex; justify-content:center; align-items: center;height:100vh;flex-direction:column; background-image: url(./cold.jpg); background-repeat: no-repeat; background-size: cover;`;
-        
-      }
+     
     });
 });
